148f99b554400d106f25c74e1a78adef77475904
Kavyan Nayak <kavyannayak104@gmail.com>
2026-07-31 09:44:40 +0530

    variables

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 // console 
 
-console.log("Hello, World!");   
-console.log("Will meet on Friday at 9 AM.");
-console.log("HW print the bio data on the console.");
+// console.log("Hello, World!");   
+// console.log("Will meet on Friday at 9 AM.");
+// console.log("HW print the bio data on the console.");
+
+
+// Browser print 
+
+// document.write("Hello, World!");
+// document.write(name);
+
+
+// variable
+
+// var 
+// let
+// const
+
+// re declare
+// var -> to declare a variable
+var name = "John";
+var name = "Palash";
+console.log(name);
+
+// re initialize 
+var age = 25;
+age = 30;
+console.log(age);
+
+
+// let  -> why use ? ->  re initialize but not re declare
+
+let city = "New York";
+city = "Los Angeles";
+console.log(city);
+
+
+// const -> why use ? -> not re initialize and not re declare
+
+const country = "USA";
+console.log(country);
