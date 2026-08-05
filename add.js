6e1d8794d1141eeceb1395c521b1245bbbb90b1f
Kavyan Nayak <kavyannayak104@gmail.com>
2026-08-05 10:02:05 +0530

    prompt & alert

--- add.js
+++ add.js
@@ -2,5 +2,9 @@ var a = 10;
 var b = 20;
 
 var c = a + b;
+var x = a - b;
 
 console.log(c);
+// console.log(x);
+document.write("<h1>" + c + "</h1>");
+// document.write(x);
---
+++ userinput.js
@@ -0,0 +1,37 @@
+// alert 
+
+// alert("Hello World");
+// alert("This is an alert message.");
+
+// prompt -> why use ? -> to take input from user    -> def -> as string
+
+// var input = prompt("Enter your name:");
+// alert(input);
+// document.write(input);
+// console.log (input);
+
+
+var a = prompt("Enter the first number:");
+var b = prompt("Enter the second number:");
+
+
+//  + -> concatenation -> string + string = string
+// var sum = a + b;
+// var sum = a * b;
+// var sum = a - b;
+// var sum = a / b;
+// alert(sum);
+
+
+// data type
+/*  
+    -> Numbers
+    -> parseInt() -> to convert string to number
+    -> parseFloat() -> to convert string to decimal number
+*/
+
+
+var sum = parseInt(a) + parseInt(b);
+
+document.write("<h1>" + a + " + " + b + "  =  " + sum + "</h1>");
+// document.write("<h1>" + sum + "</h1>");
