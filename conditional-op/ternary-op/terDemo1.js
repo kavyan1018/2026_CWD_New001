10466833e7e776f57124c212096d2a4c776c76ee
Kavyan Nayak <kavyannayak104@gmail.com>
2026-08-12 09:52:51 +0530

    ternary op

---
+++ conditional-op/ternary-op/terDemo1.js
@@ -0,0 +1,21 @@
+/*
+
+        if(cond){
+            // code 
+        }
+        else{
+            // code 
+        }
+
+
+
+        ternary: 
+
+            condition ? expression1 : expression2
+            condition ? True : False
+*/
+
+
+let age = 10;
+let result = age >= 18 ? "Adult" : "Child";
+console.log(result);
