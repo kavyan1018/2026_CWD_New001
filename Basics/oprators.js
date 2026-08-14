8ed3497c09ca04be3b84fd7e898f45dc0eba229f
Kavyan Nayak <kavyannayak104@gmail.com>
2026-08-14 10:12:11 +0530

    oprators

---
+++ Basics/oprators.js
@@ -0,0 +1,26 @@
+// js 1st -> js default data type is string
+
+// console.log(10 == 10);
+// console.log(10 === '10');
+
+
+// console.log(10 !=  5);
+// console.log('10' !==  10)
+
+
+// let a = 10;
+// a **= 3;   // 10 * *= 2
+
+// console.log(a);
+
+
+// let a = 10;
+// a >>= 3;
+
+// console.log(a);
+
+
+let a = 10;
+a <<= 2;
+
+console.log(a);
