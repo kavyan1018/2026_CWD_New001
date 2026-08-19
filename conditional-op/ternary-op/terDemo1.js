ddbeb6a7636c1ec7ccd29e081f4b6fd25c41ed7e
Kavyan Nayak <kavyannayak104@gmail.com>
2026-08-19 10:04:32 +0530

    task

--- conditional-op/ternary-op/terDemo1.js
+++ conditional-op/ternary-op/terDemo1.js
@@ -6,7 +6,16 @@
 
 // u are adult or not
 
-let a = 18;
-//        condition?   true        :   false      
-let res =  a >= 18 ? "u are adult" : "u are not adult";
+// let a = 18;
+// //        condition?   true        :   false      
+// let res =  a >= 18 ? "u are adult" : "u are not adult";
+// console.log(res);
+
+
+a = prompt("Enter any value :"); 
+
+let res = a >= '0' && a <= '9' ? "U entered a number" :
+          (a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z') ? "U entered a letter" :
+          "U entered a special character";
+
 console.log(res);
