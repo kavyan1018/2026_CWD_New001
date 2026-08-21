f4f70ef1ff4bc3e76faf60a5edbcb320d5a10c11
Kavyan Nayak <kavyannayak104@gmail.com>
2026-08-21 09:19:43 +0530

    if - esle

---
+++ conditional-op/if-else/ifDemo1.js
@@ -0,0 +1,19 @@
+/*
+
+        if(condition){
+            // true block code 
+        }
+        else{
+            // false block code
+        }
+*/
+
+// let age = 20;
+let age = prompt("Enter your age: ");
+
+if(age >= 18){
+    console.log("You are eligible to vote.");
+}
+else{
+    console.log("You are not eligible to vote.");
+}
