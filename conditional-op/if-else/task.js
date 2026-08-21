4c815c833a9cedbfd4ae6e0e4d99a3eb656d2dc8
Kavyan Nayak <kavyannayak104@gmail.com>
2026-08-21 09:49:01 +0530

    task

---
+++ conditional-op/if-else/task.js
@@ -0,0 +1,10 @@
+// check alphabet is vowel or consonant ? (capital)
+
+let a = prompt("Enter an alphabet: ");
+
+if(a == "a" || a == "e" || a == "i" || a == "o" || a == "u" || a == "A" || a == "E" || a == "I" || a == "O" || a == "U"){
+    console.log("You entered a vowel.");
+}
+else{
+    console.log("You entered a consonant.");
+}
