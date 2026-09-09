f14e90191a4ed8f305edef30709127da34c419a0
Kavyan Nayak <kavyannayak104@gmail.com>
2026-09-09 09:31:04 +0530

    switch

---
+++ switch/switch.js
@@ -0,0 +1,22 @@
+// weekdays print 
+
+let a = prompt(parseInt("Enter the Number to Display the Day :"))
+
+switch (a) {
+    case 1:
+            console.log("Monday");
+        break;
+
+    case 2:
+            console.log("Tuesday");
+        break;
+
+
+    case 3:
+            console.log("Wednesday");
+        break;
+
+    default:
+        console.log("Invalid Choice !!!");
+        break;
+}
